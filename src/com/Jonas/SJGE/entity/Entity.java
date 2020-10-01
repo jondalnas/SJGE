d372d92b8555409fbb258f20d4a60f284709a65d
Jonas Jensen <jdudal11@gmail.com>
2020-10-01 19:48:07 +0200

    Fixed entity so move works on any size of collider

--- src/com/Jonas/SJGE/entity/Entity.java
+++ src/com/Jonas/SJGE/entity/Entity.java
@@ -19,30 +19,32 @@ public abstract class Entity {
 	
 	public void move() {
 		if (dx != 0) {
+			dx:
 			for (int xx = dx * (dx < 0 ? -1 : 1); xx > 0; xx--) {
 				int x2 = x + xx * (dx < 0 ? -1 : 1);
 				
 				if (x2+xColOffs < 0 || x2+xColOffs+sizeD >= Tilemap.TILE_SIZE * game.tilemap.getWidth()) continue;
-				
-				if (game.tilemap.getTile((x2+xColOffs        )/Tilemap.TILE_SIZE, (y+yColOffs        )/Tilemap.TILE_SIZE).solid ||
-					game.tilemap.getTile((x2+xColOffs+sizeD-1)/Tilemap.TILE_SIZE, (y+yColOffs        )/Tilemap.TILE_SIZE).solid ||
-					game.tilemap.getTile((x2+xColOffs        )/Tilemap.TILE_SIZE, (y+yColOffs+sizeD-1)/Tilemap.TILE_SIZE).solid ||
-					game.tilemap.getTile((x2+xColOffs+sizeD-1)/Tilemap.TILE_SIZE, (y+yColOffs+sizeD-1)/Tilemap.TILE_SIZE).solid) continue;
-				
-				boolean collided = false;
-				for (Entity e : game.entities) {
-					if (e.x + e.xColOffs > x2 + xColOffs + sizeD ||
-						e.y + e.yColOffs > y + yColOffs + sizeD ||
-						e.x + e.xColOffs + e.sizeD < x2 + xColOffs ||
-						e.y + e.yColOffs + e.sizeD < y + yColOffs) continue;
-					
-					if (e.collide(this)) {
-						collided = true;
-						break;
+
+				int x0 = (x2+xColOffs)/Tilemap.TILE_SIZE;
+				int x1 = (x2+xColOffs+sizeD-1)/Tilemap.TILE_SIZE;
+				int y0 = (y+yColOffs)/Tilemap.TILE_SIZE;
+				int y1 = (y+yColOffs+sizeD-1)/Tilemap.TILE_SIZE;
+
+				for (int xt = x0; xt <= x1; xt++) {
+					for (int yt = y0; yt <= y1; yt++) {
+						if (game.tilemap.getTile(xt, yt).solid) break dx;
 					}
 				}
-				
-				if (collided) continue;
+
+				for (Entity e : game.entities) {
+					if (e.x + e.xColOffs > x2 + xColOffs + sizeD - 1 ||
+						e.y + e.yColOffs > y + yColOffs + sizeD - 1 ||
+						e.x + e.xColOffs + e.sizeD < x2 + xColOffs + 1 ||
+						e.y + e.yColOffs + e.sizeD < y + yColOffs + 1) continue;
+					
+					if (e.collide(this))
+						break dx;
+				}
 				
 				x = x2;
 				dx = 0;
@@ -50,31 +52,33 @@ public abstract class Entity {
 			}
 		}
 		
+		dy:
 		if (dy != 0) {
 			for (int yy = dy * (dy < 0 ? -1 : 1); yy > 0; yy--) {
 				int y2 = y + yy * (dy < 0 ? -1 : 1);
 				
 				if (y2+yColOffs < 0 || y2+yColOffs+sizeD >= Tilemap.TILE_SIZE * game.tilemap.getHeight()) continue;
+
+				int x0 = (x+xColOffs)/Tilemap.TILE_SIZE;
+				int x1 = (x+xColOffs+sizeD-1)/Tilemap.TILE_SIZE;
+				int y0 = (y2+yColOffs)/Tilemap.TILE_SIZE;
+				int y1 = (y2+yColOffs+sizeD-1)/Tilemap.TILE_SIZE;
 				
-				if (game.tilemap.getTile((x+xColOffs        )/Tilemap.TILE_SIZE, (y2+yColOffs        )/Tilemap.TILE_SIZE).solid ||
-					game.tilemap.getTile((x+xColOffs+sizeD-1)/Tilemap.TILE_SIZE, (y2+yColOffs        )/Tilemap.TILE_SIZE).solid ||
-					game.tilemap.getTile((x+xColOffs        )/Tilemap.TILE_SIZE, (y2+yColOffs+sizeD-1)/Tilemap.TILE_SIZE).solid ||
-					game.tilemap.getTile((x+xColOffs+sizeD-1)/Tilemap.TILE_SIZE, (y2+yColOffs+sizeD-1)/Tilemap.TILE_SIZE).solid) continue;
-				
-				boolean collided = false;
-				for (Entity e : game.entities) {
-					if (e.x + e.xColOffs > x + xColOffs + sizeD ||
-						e.y + e.yColOffs > y2 + yColOffs + sizeD ||
-						e.x + e.xColOffs + e.sizeD < x + xColOffs ||
-						e.y + e.yColOffs + e.sizeD < y2 + yColOffs) continue;
-					
-					if (e.collide(this)) {
-						collided = true;
-						break;
+				for (int xt = x0; xt <= x1; xt++) {
+					for (int yt = y0; yt <= y1; yt++) {
+						if (game.tilemap.getTile(xt, yt).solid) break dy;
 					}
 				}
 				
-				if (collided) continue;
+				for (Entity e : game.entities) {
+					if (e.x + e.xColOffs > x + xColOffs + sizeD - 1 ||
+						e.y + e.yColOffs > y2 + yColOffs + sizeD - 1 ||
+						e.x + e.xColOffs + e.sizeD < x + xColOffs + 1 ||
+						e.y + e.yColOffs + e.sizeD < y2 + yColOffs + 1) continue;
+					
+					if (e.collide(this))
+						break dy;
+				}
 				
 				y = y2;
 				dy = 0;
